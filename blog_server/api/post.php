5dc5f9e3087645ee441443f1270ab14b019efc58
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-08-27 10:57:59 -0400

    added vote.php

--- blog_server/api/post.php
+++ blog_server/api/post.php
@@ -1,25 +1,26 @@
 <?php
-// Load configuration files
-require_once('../config/config.php');
-require_once('../config/database.php');
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') 
-{
-    $requestUri= $_SERVER['REQUEST_URI'];
+   // Load configuration files
+   require_once('../config/config.php');
+   require_once('../config/database.php');
+
+   if ($_SERVER['REQUEST_METHOD'] === 'GET')
+   {
+    $requestUri = $_SERVER['REQUEST_URI'];
     $parts = explode('/', $requestUri);
     $id = end($parts);
 
     $query = "SELECT bp.*,
-    (SELECT COUNT(*) FROM post_votes WHERE post_id = bp.id AND vote_type = 'like') AS numlikes,
-    (SELECT COUNT(*) FROM post_votes WHERE post_id = bp.id AND vote_type = 'dislike') AS numDislikes
-    FROM blog_posts AS bp WHERE bp.id = ?";
+        (SELECT COUNT(*) FROM post_votes WHERE post_id = bp.id AND vote_type = 'like') AS numLikes,
+        (SELECT COUNT(*) FROM post_votes WHERE post_id = bp.id AND vote_type = 'dislike') AS numDislikes
+        FROM blog_posts AS bp WHERE bp.id = ?";
 
     $stmt = $conn->prepare($query);
     $stmt->bind_param('i', $id);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) 
+    if ($result->num_rows === 1)
     {
         $post = $result->fetch_assoc();
 
@@ -29,14 +30,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
                 'id' => $post['id'],
                 'title' => $post['title'],
                 'content' => $post['content'],
-                'date' => date("l js F Y, h:i A", strtotime($post['publish_date'])),
                 'author' => $post['author'],
-                'numLikes' => $post['numlikes'],
-                'numDislikes' => $post['numDislikes']
+                'date' => date("l jS \of F Y", strtotime($post['publish_date'])),
+                'likes' => $post['numLikes'],
+                'dislikes' => $post['numDislikes']
             ]
         ];
+
         header('Content-Type: application/json');
         echo json_encode($response);
+
     }
     else
     {
@@ -44,14 +47,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
             'status' => 'error',
             'message' => 'Post not found'
         ];
+
         header('Content-Type: application/json');
         echo json_encode($response);
+
     }
+
     $stmt->close();
     $conn->close();
-}
 
-
-
-
+   }
 ?>
